1b1355f00897d7a652afad86700b007b9d445e64
johnisaacenriquez <johnisaacenriquez@cp.prec.eng.osaka-u.ac.jp>
2024-08-22 01:55:32 +0900

    PEFT LoRA Finetuning

---
+++ src/llmtools.py
@@ -0,0 +1,32 @@
+def zero_shot_inference(dataset, model, tokenizer, idx):
+    dialogue = dataset['test'][idx]['dialogue']
+    summary = dataset['test'][idx]['summary']
+
+    prompt = f"""
+Summarize the following conversation.
+
+{dialogue}
+
+Summary:
+    """
+
+    # Input constructed prompt instead of the dialogue.
+    inputs = tokenizer(prompt, return_tensors='pt')
+    output = tokenizer.decode(
+        model.generate(
+            inputs["input_ids"], 
+            max_new_tokens=50,
+        )[0], 
+        skip_special_tokens=True
+    )
+
+    dash_line = '-'.join('' for x in range(100))
+    
+    print(dash_line)
+    print('Example ', idx)
+    print(dash_line)
+    print(f'INPUT PROMPT:\n{prompt}')
+    print(dash_line)
+    print(f'BASELINE HUMAN SUMMARY:\n{summary}')
+    print(dash_line)    
+    print(f'MODEL GENERATION - ZERO SHOT:\n{output}\n')
